c6173969ddb22e34a30a043cc3c3c8a4aad0aeb1
John Connerton <conjohnerton@gmail.com>
2019-03-27 01:54:12 +0000

    suppress mongoose findAndModify bug, reorganize code

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    seedDB     = require("./seeds"),
-    passport   = require("passport"),
-    methodOverride = require("method-override"),
-    LocalStrategy = require("passport-local"),
-    expressSession = require("express-session"),
-    Campground = require("./models/campground"),
-    User          = require("./models/user"),
-    Comment = require("./models/comment");
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    seedDB          = require("./seeds"),
+    passport        = require("passport"),
+    methodOverride  = require("method-override"),
+    LocalStrategy   = require("passport-local"),
+    expressSession  = require("express-session"),
+    Campground      = require("./models/campground"),
+    User            = require("./models/user"),
+    Comment         = require("./models/comment");
 
-// requiring routes
+// require routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     authRoutes       = require("./routes/index");
 
 // remove all campgrounds, exported from seeds.js
 // seedDB(); // seed the database 
+
+// set default express configurations
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+// set mongoose config and suppress findAndModify warning (mongoose bug)
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true} );
+mongoose.set('useFindAndModify', false);
 
 // PASSPORT CONFIGURATION
 app.use(expressSession({
@@ -37,11 +42,13 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// set req.user to be accessed in all ejs templates
 app.use((req, res, next) => {
    res.locals.currentUser = req.user;
    next();
 });
 
+// set shortcut routes
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use(authRoutes);
